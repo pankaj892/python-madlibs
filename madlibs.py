ef76d601eb0282bde913cdf93e4580607d43b49a
Pankaj biradar <pankaj28988@gmail.com>
2023-01-18 16:45:39 +0530

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+adj = input("Adjective: ")
+hero = input("Hero: ")
+verb = input("Verb: ")
+famous_person = input("Famous Person: ")
+vcs = input("Version Control System: ")
+madlib = f"Computer Programming is so {adj}. It makes me feel like {hero}. Stay hydrated and {verb} you'll surely reach there.\
+    Tech is the future.It will be the future of the world.You can do anything with it.I love {vcs}. The best thing about it is that it is free.\
+    Open Source is the best thing that has ever happened to the world.\
+    The best thing about {vcs} is that it is free."
+
+print(madlib)
